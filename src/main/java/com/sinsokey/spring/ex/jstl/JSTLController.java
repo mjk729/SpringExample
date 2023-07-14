5d752510cf7ba1be4e6bd4a69cd32821e1962848
mjk729 <kimminjung0729@naver.com>
2023-07-14 16:05:40 +0900

    [김민중] -jstl core 예제 2 파일 생성

--- src/main/java/com/sinsokey/spring/ex/jstl/JSTLController.java
+++ src/main/java/com/sinsokey/spring/ex/jstl/JSTLController.java
@@ -1,6 +1,10 @@
 package com.sinsokey.spring.ex.jstl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,5 +13,21 @@ public class JSTLController {
 	@GetMapping("/jstl/ex01")
 	public String ex01() {
 		return "jstl/ex01";
+	}
+	
+	@GetMapping("/jstl/ex02")
+	public String ex02(Model model) {
+		
+		List<String> fruitList = new ArrayList<>();
+		fruitList.add("사과");
+		fruitList.add("바나나");
+		fruitList.add("딸기");
+		fruitList.add("수박");
+		fruitList.add("망고");
+		fruitList.add("체리");
+		
+		model.addAttribute("fruitList", fruitList);
+		
+		return "jstl/ex02";
 	}
 }
